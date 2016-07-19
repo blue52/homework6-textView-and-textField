b4bf34d054e8954c2fddcea91dc8f9aee294566d
藍仕凱 <freebluesky66@gmail.com>
2016-07-19 16:59:01 +0800

    first

--- homework6/ViewController.swift
+++ homework6/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    @IBOutlet weak var displaywordsLabel: UILabel!
 
+    @IBOutlet weak var displayTextViewLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
+        if range.location >= 10 {
+            
+            return false
+            
+        }else{
+            displaywordsLabel.text = String(range.location + 1)
+            return true
+            
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if range.location >= 10{
+            textView.resignFirstResponder()
+            return false
+        }else{
+            displayTextViewLabel.text = String(range.location + 1)
+            return true
+        }
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
